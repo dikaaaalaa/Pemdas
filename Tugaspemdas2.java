ef57a721af6c26bb546d2d7c8a3b7b1bcd7a0fb0
dikaaaalaa <164788792+dikaaaalaa@users.noreply.github.com>
2025-09-13 18:37:17 +0700

    Update Tugaspemdas2.java

--- Tugaspemdas2.java
+++ Tugaspemdas2.java
@@ -4,30 +4,34 @@ public class Tugaspemdas2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        //menghitung IMT
         System.out.println("menghitung Indeks massa tubuh (IMT)");
+        //memasukkan berat dan tinggi
         System.out.print("masukkan berat badan (kg): ");
         double berat = input.nextDouble();
         System.out.print("masukkan tinggi badan (m): ");
+        // menghitung dengan rumus IMT
         double tinggi = input.nextDouble();
         double imt = berat / (tinggi * tinggi);
-
+        
+        //menampilkan output
         System.out.printf("Indeks massa tubuh (IMT) = %.2f\n", imt);
 
-        if (imt < 18.5){
+        //menentukan keterangan berdasarkan nilai IMT
+        if (imt <= 18.5){
             System.out.println("keterangan: kurus");
         }
-        else if (imt < 18.5 && imt < 25){
+        else if (imt < 18.5 & imt <= 25){
             System.out.println("keterangan: normal");
         }
-        else if (imt < 25 && imt < 30){
+        else if (imt < 25 & imt <= 30){
             System.out.println("keterangan: gemuk");
         }
-        else {
+        else if(imt > 30){
             System.out.println("keterangan: kegemukan");
 
         }
-    
-        input.close();
+        //menutup scanner
    input.close();
     } 
 }
